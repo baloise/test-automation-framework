93e92f281d91f54e256ca3dabcb04d0e238dd258
Christian Josephy <christian.josephy@baloise.ch>
2016-02-19 15:23:18 +0100

    Added convenience-method

--- taf-browser/src/main/java/com/baloise/testautomation/taf/browser/elements/ABrRadiobutton.java
+++ taf-browser/src/main/java/com/baloise/testautomation/taf/browser/elements/ABrRadiobutton.java
@@ -15,6 +15,17 @@ import com.baloise.testautomation.taf.base._interfaces.IRadiobutton;
  */
 public class ABrRadiobutton extends ABrInput implements IRadiobutton {
 
+  public Boolean fillValueAsPrimitiveBoolean() {
+    if (fillValue.isSkip() || fillValue.isEmpty()) {
+      return false;
+    }
+    Boolean b = fillValue.asBoolean();
+    if (b == null) {
+      return false;
+    }
+    return b.booleanValue();
+  }
+
   @Override
   public boolean isSelected() {
     return find().isSelected();
