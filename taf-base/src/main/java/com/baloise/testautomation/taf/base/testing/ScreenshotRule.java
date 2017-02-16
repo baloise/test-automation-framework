4ff127146b338371688e27f48f65580a1cd8fca9
Christian Josephy <christian.josephy@baloise.ch>
2017-02-16 08:12:32 +0100

    New Screenshot Rule

---
+++ taf-base/src/main/java/com/baloise/testautomation/taf/base/testing/ScreenshotRule.java
@@ -0,0 +1,89 @@
+/*
+ ===========================================================================
+ @    $Author$
+ @  $Revision$
+ @      $Date$
+ @
+ ===========================================================================
+ */
+package com.baloise.testautomation.taf.base.testing;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 
+ */
+public class ScreenshotRule extends TestWatcher {
+
+  public static Logger logger = LoggerFactory.getLogger("ScreenshotOnFailed");
+
+  private String path = "";
+  private boolean onBefore = false;
+  private boolean onAfter = false;
+  private boolean onFailed = false;
+
+  public ScreenshotRule(String path, boolean onBefore, boolean onAfter, boolean onFailed) {
+    super();
+    this.path = path;
+    this.path = this.path.replace("\\", "/");
+    if (this.path.length() > 0) {
+      if (!this.path.endsWith("/")) {
+        this.path = this.path + "/";
+      }
+    }
+    this.onBefore = onBefore;
+    this.onAfter = onAfter;
+    this.onFailed = onFailed;
+  }
+
+  @Override
+  protected void failed(Throwable e, Description description) {
+    if (onFailed) {
+      saveScreenShot("failed_" + System.currentTimeMillis() + "_" + description.getMethodName() + ".jpg");
+    }
+  }
+
+  public void saveScreenShot() {
+    saveScreenShot("failed_" + System.currentTimeMillis() + ".jpg");
+  }
+
+  public void saveScreenShot(String filename) {
+    logger.info("Execute saveScreenShot()");
+    try {
+      Toolkit toolkit = Toolkit.getDefaultToolkit(); // Toolkit class returns the default toolkit
+      Dimension dimension = toolkit.getScreenSize();
+      Rectangle rectangle = new Rectangle(0, 0, dimension.width, dimension.height);
+      Robot robot = new Robot();
+      BufferedImage image = robot.createScreenCapture(rectangle);
+      File f = new File(path + filename);
+      ImageIO.write(image, "jpg", f);
+      logger.info("Done saveScreenShot() " + f);
+    }
+    catch (Exception ex) {
+      logger.error("Error when saving image: " + ex.getMessage());
+    }
+  }
+
+  protected void starting(Description description) {
+    if (onBefore) {
+      saveScreenShot("before_" + System.currentTimeMillis() + ".jpg");
+    }
+  }
+
+  protected void finished(Description description) {
+    if (onAfter) {
+      saveScreenShot("after_" + System.currentTimeMillis() + ".jpg");
+    }
+  }
+
+}
