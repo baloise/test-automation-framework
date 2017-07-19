93c23ac4d22dafb5007df4487a96ade10ce0306d
Joachim Prinzbach <joachim.prinzbach@gmail.com>
2017-07-19 23:52:41 +0200

    Add Json Data reader.

--- taf-base/src/main/java/com/baloise/testautomation/taf/base/json/JsonDataImporter.java
+++ taf-base/src/main/java/com/baloise/testautomation/taf/base/json/JsonDataImporter.java
@@ -1,53 +1,78 @@
 package com.baloise.testautomation.taf.base.json;
 
+import com.baloise.testautomation.taf.base._base.DataRow;
 import com.baloise.testautomation.taf.base._interfaces.IDataImporter;
 import com.baloise.testautomation.taf.base._interfaces.IDataRow;
 import com.baloise.testautomation.taf.base.types.TafId;
+import com.baloise.testautomation.taf.base.types.TafString;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Collection;
-import java.util.Iterator;
+import java.util.*;
 
 import static org.junit.Assert.assertNotNull;
 
 public class JsonDataImporter implements IDataImporter {
 
-    private final File file;
+    private List<JsonNode> rows = new ArrayList<>();
+    private List<String> idFields = Arrays.asList("mandant", "id", "detail");
 
     public JsonDataImporter(File file) {
-        this.file = file;
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            JsonNode rootNode = mapper.readTree(file);
+            Iterator<JsonNode> elementIterator = rootNode.elements();
+            while (elementIterator.hasNext()) {
+                rows.add(elementIterator.next());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         assertNotNull("input stream may not be null", file);
     }
 
 
     @Override
     public int getNrOfDataRowsWith(TafId tarifId) {
-        ObjectMapper mapper = new ObjectMapper();
-        int rows = 0;
-        try {
-            JsonNode rootNode = mapper.readTree(this.file);
-            Iterator<JsonNode> elementIterator = rootNode.elements();
-            while (elementIterator.hasNext()) {
-                JsonNode element = elementIterator.next();
-                String mandant = element.get("mandant").asText();
-                String id = element.get("id").asText();
-                String detail = element.get("detail").asText();
-                TafId recordId = new TafId(mandant, id, detail, null);
-                if (recordId.equals(tarifId)) {
-                    rows++;
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return rows;
+        assertNotNull("no JSON records found", rows);
+        return getWith(tarifId).size();
     }
 
     @Override
-    public Collection<IDataRow> getWith(TafId id) {
-        return null;
+    public Collection<IDataRow> getWith(TafId tafId) {
+        List<JsonNode> matchingRecords = new ArrayList<>();
+        for (JsonNode element : rows) {
+            TafId recordId = getTafId(element);
+            if (recordId.equals(tafId)) {
+                matchingRecords.add(element);
+            }
+        }
+        return convertToDataRow(matchingRecords);
+    }
+
+    private Collection<IDataRow> convertToDataRow(List<JsonNode> records) {
+        List<IDataRow> data = new ArrayList<IDataRow>();
+        for (JsonNode record : records) {
+            Iterator<Map.Entry<String, JsonNode>> fields = record.fields();
+            DataRow dataRow = new DataRow();
+            dataRow.setId(getTafId(record));
+            while (fields.hasNext()) {
+                Map.Entry<String, JsonNode> next = fields.next();
+                if (!idFields.contains(next.getKey())) {
+                    dataRow.set(next.getKey(), TafString.normalString(next.getValue().asText()));
+                }
+            }
+            data.add(dataRow);
+        }
+        return data;
+    }
+
+    private TafId getTafId(JsonNode element) {
+        String mandant = element.get("mandant").asText();
+        String id = element.get("id").asText();
+        String detail = element.get("detail").asText();
+        return new TafId(mandant, id, detail, null);
     }
 }
--- taf-base/src/test/java/com/baloise/testautomation/taf/base/TestJsonDataLoader.java
+++ taf-base/src/test/java/com/baloise/testautomation/taf/base/TestJsonDataLoader.java
@@ -1,16 +1,24 @@
 package com.baloise.testautomation.taf.base;
 
+import com.baloise.testautomation.taf.base._interfaces.IDataRow;
 import com.baloise.testautomation.taf.base.csv.CsvDataImporter;
 import com.baloise.testautomation.taf.base.json.JsonDataImporter;
 import com.baloise.testautomation.taf.base.types.TafId;
+import com.baloise.testautomation.taf.base.types.TafString;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Vector;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 public class TestJsonDataLoader {
 
@@ -19,7 +27,7 @@ public class TestJsonDataLoader {
 
     @BeforeClass
     public static void init() throws URISyntaxException {
-        file = new File(TestCsvDataLoader.class.getResource("TestJsonDataLoader.json").toURI());
+        file = new File(TestJsonDataLoader.class.getResource("TestJsonDataLoader.json").toURI());
     }
 
     @Before
@@ -32,6 +40,80 @@ public class TestJsonDataLoader {
         assertEquals(1, jsonDataImporter.getNrOfDataRowsWith(new TafId("int", "test", "1")));
         assertEquals(1, jsonDataImporter.getNrOfDataRowsWith(new TafId("int", "TEST", "2")));
         assertEquals(0, jsonDataImporter.getNrOfDataRowsWith(new TafId("inti", "TEST", "2")));
+    }
+
+    @Test
+    public void openWithFile() {
+        assertEquals(1, jsonDataImporter.getNrOfDataRowsWith(new TafId("int", "test", "1")));
+    }
+
+    @Test
+    public void rowContents1() {
+        Collection<IDataRow> data = jsonDataImporter.getWith(new TafId("int", "TEst", "1"));
+        assertEquals(1, data.size());
+        Vector<IDataRow> vData = new Vector<>();
+        vData.addAll(data);
+        assertEquals(TafString.class, vData.get(0).get("INTEGER").getClass());
+        assertEquals(TafString.class, vData.get(0).get("double").getClass());
+        assertEquals(TafString.class, vData.get(0).get("date").getClass());
+        assertEquals(TafString.class, vData.get(0).get("string").getClass());
+        assertEquals(TafString.class, vData.get(0).get("boolean").getClass());
+    }
+
+    @Test
+    public void rowContents2() {
+        Collection<IDataRow> data = jsonDataImporter.getWith(new TafId("int", "TEst", "2"));
+        assertEquals(1, data.size());
+        Vector<IDataRow> vData = new Vector<>();
+        vData.addAll(data);
+        assertEquals(2, vData.get(0).get("INTEGER").asInteger().intValue());
+        assertEquals(new Double(2.2), vData.get(0).get("Double").asDouble());
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        try {
+            System.out.println(vData.get(0).get("date").asDate());
+            assertEquals(sdf.parse("02.02.2000"), vData.get(0).get("date").asDate());
+        }
+        catch (ParseException e) {
+            fail();
+        }
+        assertEquals("Zwei String", vData.get(0).get("string").asString());
+        assertEquals(false, vData.get(0).get("boolean").asBoolean());
+    }
+
+    @Test
+    public void rowContents3() throws URISyntaxException {
+        Collection<IDataRow> data = jsonDataImporter.getWith(new TafId("test", "othertest", ""));
+        assertEquals(1, data.size());
+        Vector<IDataRow> vData = new Vector<>();
+        vData.addAll(data);
+        assertTrue(vData.get(0).get("INTEGER").isNull());
+        assertTrue(vData.get(0).get("double").isSkip());
+        assertTrue(vData.get(0).get("date").isEmpty());
+        assertEquals(TafString.class, vData.get(0).get("string").getClass());
+        assertEquals(TafString.class, vData.get(0).get("boolean").getClass());
+
+    }
+
+    @Test
+    public void rowContents4() {
+        Collection<IDataRow> data = jsonDataImporter.getWith(new TafId("test", "string", ""));
+        assertEquals(1, data.size());
+        Vector<IDataRow> vData = new Vector<>();
+        vData.addAll(data);
+        assertTrue(vData.get(0).get("INTEGER") instanceof TafString);
+        assertTrue(vData.get(0).get("double") instanceof TafString);
+
+    }
+
+    @Test
+    public void rowTafId() {
+        Collection<IDataRow> data = jsonDataImporter.getWith(new TafId("int", "TEst", ""));
+        assertEquals(2, data.size());
+        Vector<IDataRow> vData = new Vector<>();
+        vData.addAll(data);
+        System.out.println(vData.get(0).getId().asIdDetailString());
+        assertTrue(vData.get(0).getId().asIdDetailString().equalsIgnoreCase("test-1"));
+        assertTrue(vData.get(1).getId().asIdDetailString().equalsIgnoreCase("test-2"));
     }
 
 }
