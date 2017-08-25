54db82b1b211062508c2296fd54abdcc47a6bcdb
Christian Josephy <christian.josephy@baloise.ch>
2017-08-25 08:16:03 +0200

    Implemented a patched firefox driver to overcome clicking problems with latest version

---
+++ taf-browser/src/main/java/com/baloise/testautomation/taf/browser/patchedfirefox/PatchedFirefoxDriver.java
@@ -0,0 +1,34 @@
+package com.baloise.testautomation.taf.browser.patchedfirefox;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+public class PatchedFirefoxDriver extends FirefoxDriver {
+
+  public PatchedFirefoxDriver(FirefoxOptions ffo) {
+    super(ffo);
+  }
+
+  @Override
+  public List<WebElement> findElements(By by) {
+    List<WebElement> elements = super.findElements(by);
+    List<WebElement> patchedElements = new ArrayList<WebElement>();
+    for (WebElement element : elements) {
+      patchedElements.add(new PatchedWebElement((RemoteWebElement)element));
+    }
+    return patchedElements;
+  }
+
+  @Override
+  public WebElement findElement(By by) {
+    return new PatchedWebElement((RemoteWebElement)super.findElement(by));
+  }
+
+
+}
---
+++ taf-browser/src/main/java/com/baloise/testautomation/taf/browser/patchedfirefox/PatchedWebElement.java
@@ -0,0 +1,35 @@
+package com.baloise.testautomation.taf.browser.patchedfirefox;
+
+import java.lang.reflect.Field;
+
+import org.junit.Assert;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+public class PatchedWebElement extends RemoteWebElement {
+
+  public PatchedWebElement(RemoteWebElement rwe) {
+    Field[] rweFields = RemoteWebElement.class.getDeclaredFields();
+    for (Field rweField : rweFields) {
+      try {
+        rweField.setAccessible(true);
+        Field localField = RemoteWebElement.class.getDeclaredField(rweField.getName());
+        localField.set(this, rweField.get(rwe));
+      }
+      catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+        e.printStackTrace();
+        Assert.fail("could not create patched web element");
+      }
+    }
+  }
+  
+  @Override
+  // with firefox 55 and selenium 3.5.2, clicking is behaving differently from selenium 2.53.0 and firefox 45
+  // hovering to an element before clicking seems to work --> override click functionality
+  public void click() {
+    Actions a = new Actions(parent);
+    a.moveToElement(this).perform();
+    super.click();
+  }
+
+}
