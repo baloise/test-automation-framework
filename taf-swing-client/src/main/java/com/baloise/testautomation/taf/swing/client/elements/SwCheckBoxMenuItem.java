1fc5fc062550f81f4721d1dd9b28bb2401eb78a5
Markus Tiede <markus.tiede@open-co.de>
2018-09-25 15:50:38 +0200

    Unused import fix

--- taf-swing-client/src/main/java/com/baloise/testautomation/taf/swing/client/elements/SwCheckBoxMenuItem.java
+++ taf-swing-client/src/main/java/com/baloise/testautomation/taf/swing/client/elements/SwCheckBoxMenuItem.java
@@ -2,7 +2,6 @@ package com.baloise.testautomation.taf.swing.client.elements;
 
 import com.baloise.testautomation.taf.base._base.AElement;
 import com.baloise.testautomation.taf.swing.base._interfaces.ISwCheckBoxMenuItem;
-import com.baloise.testautomation.taf.swing.base._interfaces.ISwMenuItem;
 
 public class SwCheckBoxMenuItem extends AElement{
 
