5b0cc36273e1faa3bcc27ab7d1c2254160f4fa5f
Christian Josephy <christian.josephy@baloise.ch>
2020-07-29 17:16:26 +0200

    Fixed Tests
    testcases switched to TafError

--- taf-browser/src/test/java/com/baloise/testautomation/taf/browser/elements/BrIntegerInputTest.java
+++ taf-browser/src/test/java/com/baloise/testautomation/taf/browser/elements/BrIntegerInputTest.java
@@ -1,12 +1,14 @@
 package com.baloise.testautomation.taf.browser.elements;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import com.baloise.testautomation.taf.base._base.TafError;
 import com.baloise.testautomation.taf.base._interfaces.IAnnotations;
 import com.baloise.testautomation.taf.base.types.TafInteger;
 import com.baloise.testautomation.taf.browser.TafBrowserTest;
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 public class BrIntegerInputTest extends TafBrowserTest {
 
@@ -27,7 +29,7 @@ public class BrIntegerInputTest extends TafBrowserTest {
     try {
       integerInput.setFill("bubu");
       integerInput.fill();
-    } catch (RuntimeException e) {
+    } catch (TafError e) {
       assertEquals("illegal number format", e.getMessage());
     }
   }
