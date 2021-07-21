d5b59803cf7dd1ab304a9ef34cf84ae4403dffcc
Leber, Mike <mike.leber@baloise.ch>
2021-07-21 13:56:06 +0200

    Improved errorhandling for button click

--- taf-base/src/main/java/com/baloise/testautomation/taf/base/_base/TafError.java
+++ taf-base/src/main/java/com/baloise/testautomation/taf/base/_base/TafError.java
@@ -1,9 +1,33 @@
 package com.baloise.testautomation.taf.base._base;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+
 public class TafError extends Error {
 
-  public TafError(String msg) {
-    super(msg);
-  }
+    private Annotation byAnnotation;
+    public TafError(String msg) {
+        super(msg);
+    }
+    public TafError(String msg,Annotation causeAnnotation) {
+        super(msg);
+        byAnnotation=causeAnnotation;
+    }
 
+    public static String getByInfo(Annotation aAnnotation) {
+      if (aAnnotation!=null){
+        try {
+            Method valueMethod = aAnnotation.getClass().getMethod("value");
+            if (valueMethod != null)
+                return " is caused byAnnotation:" + aAnnotation.getClass().getInterfaces()[0].getSimpleName() + " -> " + valueMethod.invoke(aAnnotation);
+        } catch (Exception e) {
+        }
+      }
+      return "";
+    }
+
+  @Override
+  public String toString() {
+    return super.toString()+ getByInfo(byAnnotation);
+  }
 }
