511d866a457d9c62028ea1e22ae852d50fe107d5
Leber, Mike <mike.leber@baloise.ch>
2021-08-10 16:40:27 +0200

    Apply Codestyle

--- taf-base/src/main/java/com/baloise/testautomation/taf/base/_base/TafError.java
+++ taf-base/src/main/java/com/baloise/testautomation/taf/base/_base/TafError.java
@@ -5,36 +5,40 @@ import java.lang.reflect.Method;
 
 public class TafError extends Error {
 
-    private Annotation byAnnotation;
+    private Annotation byAnnotation = null;
+
     public TafError(String msg) {
-        super(msg);
+        this(msg, null);
     }
-    public TafError(String msg,Annotation causeAnnotation) {
+
+    public TafError(String msg, Annotation causeAnnotation) {
         super(msg);
-        byAnnotation=causeAnnotation;
+        byAnnotation = causeAnnotation;
     }
 
     public static String getByInfo(Annotation aAnnotation) {
-      if (aAnnotation!=null){
-        try {
-            Method valueMethod = aAnnotation.getClass().getMethod("value");
-            if (valueMethod != null)
-                return " is caused byAnnotation:" + aAnnotation.getClass().getInterfaces()[0].getSimpleName() + " -> " + valueMethod.invoke(aAnnotation);
-        } catch (Exception e) {
+        if (aAnnotation != null) {
+            try {
+                Method valueMethod = aAnnotation.getClass().getMethod("value");
+                if (valueMethod != null)
+                    return " is caused byAnnotation: " + aAnnotation.getClass().getInterfaces()[0].getSimpleName() + " -> " + valueMethod.invoke(aAnnotation);
+            } catch (Exception e) {
+            }
         }
-      }
-      return "";
+        return "";
     }
+
     public String getByInfo() {
         return getByInfo(byAnnotation);
     }
+
     @Override
     public String getMessage() {
-        return super.getMessage()+getByInfo();
+        return super.getMessage() + getByInfo();
     }
 
     @Override
-  public String toString() {
-    return super.toString()+ getByInfo(byAnnotation);
-  }
+    public String toString() {
+        return super.toString() + getByInfo(byAnnotation);
+    }
 }
