98f168fad242b7adf045abf9821e7ef6d455837b
Pascal Bürklin <pascal@buerkl.in>
2021-11-30 15:49:36 +0100

    Revert parent finding

--- taf-base/src/main/java/com/baloise/testautomation/taf/base/_base/AElement.java
+++ taf-base/src/main/java/com/baloise/testautomation/taf/base/_base/AElement.java
@@ -84,6 +84,8 @@ public abstract class AElement implements IElement {
     fail("element after " + seconds + " seconds not found: " + name);
   }
 
+  public abstract Object find();
+
   public Object brFind() {
     assertComponentNotNull();
     Object we = null;
@@ -91,11 +93,7 @@ public abstract class AElement implements IElement {
       we = brFindByCustom();
     }
     if (we == null) {
-      Object parent = null;
-      if (this.component != null) {
-        parent = this.component.find();
-      }
-      we = component.getBrowserFinder().find(parent, by);
+      we = component.getBrowserFinder().find(by);
     }
     assertNotNull("webelement NOT found: " + name + TafError.getByInfo(by), we);
     return we;
