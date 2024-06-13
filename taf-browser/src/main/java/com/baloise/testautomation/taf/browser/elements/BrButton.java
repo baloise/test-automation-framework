3fb57898a0b16d074002a81d903faaa0aac78b56
Christian Josephy <christian.josephy@baloise.ch>
2024-06-13 17:13:40 +0200

    getDriver for Buttons too

--- taf-browser/src/main/java/com/baloise/testautomation/taf/browser/elements/BrButton.java
+++ taf-browser/src/main/java/com/baloise/testautomation/taf/browser/elements/BrButton.java
@@ -1,9 +1,11 @@
 package com.baloise.testautomation.taf.browser.elements;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import com.baloise.testautomation.taf.base._base.AButton;
 import com.baloise.testautomation.taf.base._base.TafError;
+import com.baloise.testautomation.taf.common.interfaces.IFinder;
 
 public class BrButton extends AButton {
 
@@ -15,6 +17,14 @@ public class BrButton extends AButton {
     @Override
     public WebElement find() {
         return (WebElement) brFind();
+    }
+
+    public WebDriver getDriver() {
+      IFinder<?> finder = getFinder();
+      if (finder instanceof BrFinder) {
+        return ((BrFinder)finder).getDriver();
+      }
+      return null;
     }
 
 }
